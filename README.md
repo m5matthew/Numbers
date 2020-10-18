f9f52e16da5379d70c67aa8d8b02e817b2d0becc
Matthew Wang <matthew.wang.c@gmail.com>
2020-10-18 14:50:16 -0700

    Documented an alternative approach

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Numbers
+
 Flask app to display even, odd and prime numbers
+
+# Alternative approach
+
+Another approach would be to define a function for each method
+'''Python3
+@app.route('/<int:number>')
+def print_numbers(number):
+...
+
+@app.route('/<int:number>/even')
+def print_evens(number):
+...
+
+@app.route('/<int:number>/odd>')
+def print_odd(number):
+...
+
+@app.route('/<int:number>/prime')
+def print_primes(number):
+...
+'''
+
+**Pros**
+
+- Easier to see which endpoints are defined
+- Easier to unit test
+  **Cons**
+- Repetitive
+- Code is cluttered
--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask
-from numberlib import is_odd, is_even, is_prime
+from lib import numberlib
 app = Flask(__name__)
 
 
 @app.route('/<int:number>')
 @app.route('/<int:number>/<arg>')
-def numbers(number, arg=None):
-
+def print_numbers(number, arg=None):
     out = list(range(1, number+1))
 
     if arg == "odd":
-        out = filter(is_odd, out)
+        out = filter(numberlib.is_odd, out)
 
     elif arg == "even":
-        out = filter(is_even, out)
+        out = filter(numberlib.is_even, out)
 
     elif arg == "prime":
-        out = filter(is_prime, out)
+        out = filter(numberlib.is_prime, out)
 
     # Return sequence of numbers joined with commas
     return ','.join([str(x) for x in out])
