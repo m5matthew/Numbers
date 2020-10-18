dabce4612d00ac36491f86a780e5b74a64651ab3
Matthew Wang <matthew.wang.c@gmail.com>
2020-10-18 13:49:14 -0700

    Approach 1: One big function

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/<int:number>/<arg>')
+def numbers(number, arg):
+
+    def is_prime(x):
+
+        # negative numbers and {0,1} are not prime numbers
+        if x < 2:
+            return False
+
+        for i in range(2, x):
+            if x % i == 0:
+                return False
+        return True
+
+    if arg == "odd":
+        return ','.join([str(x) for x in range(1, number+1) if x % 2 == 1])
+
+    elif arg == "even":
+        return ','.join([str(x) for x in range(1, number+1) if x % 2 == 0])
+
+    elif arg == "prime":
+        return ','.join([str(x) for x in range(1, number+1) if is_prime(x)])
+
+    return f"Argument {arg} is not supported. Please select from odd, even, or prime"
